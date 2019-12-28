4be0ddb03fde872e4a9c4d89df3cfacb8623d5e4
SanghyeonLee <lsh1107@kaist.ac.kr>
2019-12-28 11:10:56 +0900

    Create PictureDetail.java

---
+++ app/src/main/java/com/example/madcamp_week1/Picture_detail.java
@@ -0,0 +1,14 @@
+package com.example.madcamp_week1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Picture_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_detail);
+    }
+}
