899d2110ff831cbd9463c10ab95ff7a23765a07f
SanghyeonLee <lsh1107@kaist.ac.kr>
2019-12-28 23:22:44 +0900

    Test Parsing json file

--- app/src/main/java/com/example/madcamp_week1/Contact_view.java
+++ app/src/main/java/com/example/madcamp_week1/Contact_view.java
@@ -4,20 +4,101 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.graphics.Movie;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
 public class Contact_view extends Fragment {
+
+    ArrayList<Contact> contactList = new ArrayList<>();
+
+    public class Contact{
+        private String name;
+        private String phone_number;
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPhone_number() {
+            return phone_number;
+        }
+
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setPhone_number(String phone_number) {
+            this.phone_number = phone_number;
+        }
+
+    }
 
 
     public static Contact_view newInstance() {
         Contact_view fragmentFirst = new Contact_view();
         return fragmentFirst;
     }
+
+    public String getJsonString()
+    {
+        String json = "";
+
+        try {
+            InputStream is = getResources().getAssets().open("data.json");
+            int fileSize = is.available();
+
+            byte[] buffer = new byte[fileSize];
+            is.read(buffer);
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+
+        return json;
+    }
+
+    public void jsonParsing(String json)
+    {
+        try{
+            JSONObject jsonObject = new JSONObject(json);
+
+            JSONArray movieArray = jsonObject.getJSONArray("Contact");
+
+            for(int i=0; i<movieArray.length(); i++)
+            {
+                JSONObject contactObject = movieArray.getJSONObject(i);
+
+                Contact contact = new Contact();
+
+                contact.setName(contactObject.getString("name"));
+                contact.setPhone_number(contactObject.getString("phone_number"));
+
+                contactList.add(contact);
+            }
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
