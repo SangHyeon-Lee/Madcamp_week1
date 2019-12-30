a8dd54c5495fb2c7395c68e12a32b4d8904e22eb
SeOk Jeon <jso4967@gmail.com>
2019-12-30 14:01:00 +0900

    Create Music Start, Pause feature

--- app/src/main/java/com/example/madcamp_week1/Music_view.java
+++ app/src/main/java/com/example/madcamp_week1/Music_view.java
@@ -4,12 +4,21 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class Music_view extends Fragment {
+public class Music_view extends Fragment implements View.OnClickListener {
+
+    MediaPlayer mediaPlayer;
+    Button btnStart, btnStop;
+
 
     public static Music_view newInstance() {
         Music_view fragmentFirst = new Music_view();
@@ -29,7 +38,33 @@ public class Music_view extends Fragment {
             @Nullable Bundle savedInstanceState
     ) {
         View view = inflater.inflate(R.layout.activity_music_view, container, false);
+        btnStart = view.findViewById(R.id.btn_music_start);
+        btnStop = view.findViewById(R.id.btn_music_stop);
+        btnStart.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
         return view;
     }
-    
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btn_music_start:
+                mediaPlayer = MediaPlayer.create(this.getContext(), R.raw.high);
+                mediaPlayer.start();
+                break;
+            case R.id.btn_music_stop:
+                mediaPlayer.stop();
+                mediaPlayer.reset();
+                break;
+        }
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        if(mediaPlayer != null){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
 }
