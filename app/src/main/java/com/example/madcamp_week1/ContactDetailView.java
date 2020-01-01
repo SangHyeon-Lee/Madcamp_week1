138fd88679fef975e18eeb5316bbf18d6f634061
SeOk Jeon <jso4967@gmail.com>
2020-01-01 15:36:34 +0900

    Update ContactDetailLayout & itemOnClickListener to connect item with DetailView

---
+++ app/src/main/java/com/example/madcamp_week1/ContactDetailView.java
@@ -0,0 +1,14 @@
+package com.example.madcamp_week1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactDetailView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_detail_view);
+    }
+}
